64f10fe66515ba752403a281d66ae09f85743dcb
Mirza Showvik <showvikmirza@gmail.com>
2023-05-25 16:42:30 +0600

    Initial Commit

---
+++ Tenders Guru/Tenders_GuruApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tenders_GuruApp.swift
+//  Tenders Guru
+//
+//  Created by Mirza Showvik on 25/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Tenders_GuruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
